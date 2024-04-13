24bd9f1fa38bbb762f0e5bfa045f199e19c11cb7
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2024-04-13 12:35:13 -0400

    connected database

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -92,6 +92,14 @@ DJOSER = {
 
 # TODO здесь необходимо настроить подключение к БД
 DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
 
 

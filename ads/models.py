650d3ac76fdfa4a2b24de3c3e4844e03b2012f7c
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2024-04-15 21:43:54 -0400

    added changes to User model and everything related to it

--- ads/models.py
+++ ads/models.py
@@ -1,10 +1,24 @@
-from django.conf import settings
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
+NULLABLE = {'blank': True, 'null': True}
 
 
-class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+class Product(models.Model):
+    image = models.ImageField(upload_to="images/", verbose_name="product image", **NULLABLE)
+    title = models.CharField(max_length=255, verbose_name="product name")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="product price")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, verbose_name='product author', **NULLABLE)
+    created_at = models.DateField(default=timezone.now, verbose_name="product creation date")
+    description = models.TextField(verbose_name="product description")
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "product"
+        verbose_name_plural = "products"
 
 
 class Comment(models.Model):
--- users/models.py
+++ users/models.py
@@ -1,8 +1,45 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 NULLABLE = {'null': True, 'blank': True}
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+
+        return self.create_user(email, password, **extra_fields)
+
+    def get_by_natural_key(self, email):
+        """
+        Retrieve a user by their email (natural key).
+        """
+        return self.get(email=email)
 
 
 class UserRoles(models.TextChoices):
@@ -12,7 +49,7 @@ class UserRoles(models.TextChoices):
     ADMIN = 'admin', _('admin')
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     username = None
     first_name = models.CharField(max_length=255, verbose_name="first name")
     last_name = models.CharField(max_length=255, verbose_name="last name")
@@ -20,15 +57,27 @@ class User(AbstractBaseUser):
     phone = models.CharField(max_length=20, verbose_name="phone number", **NULLABLE)
     city = models.CharField(max_length=35, verbose_name="city", **NULLABLE)
     avatar = models.ImageField(upload_to="users/", verbose_name="avatar", **NULLABLE)
-    role = models.CharField(max_length=5, choices=UserRoles.choices, default=UserRoles.USER)
+    role = models.CharField(max_length=20, choices=UserRoles.choices, default=UserRoles.USER)
+    is_staff = models.BooleanField(default=False, verbose_name="staff status")
+    is_superuser = models.BooleanField(default=False, verbose_name="superuser status")
+
+    objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return self.email
 
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        return True
+
     class Meta:
         verbose_name = 'user'
         verbose_name_plural = 'users'
-

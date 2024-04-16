c5e5da658e624426395b74825d3662de0d1b081b
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2024-04-16 10:34:22 -0400

    added models serializers

--- ads/serializers.py
+++ ads/serializers.py
@@ -1,18 +1,27 @@
 from rest_framework import serializers
+from .models import Comment, Ad
+from ..users.serializers import UserSerializer
 
-
-# TODO Сериалайзеры. Предлагаем Вам такую структуру, однако вы вправе использовать свою
 
 class CommentSerializer(serializers.ModelSerializer):
-    # TODO сериалайзер для модели
-    pass
+    """ Used to view comments """
+    model = Comment
+    fields = '__all__'
 
 
 class AdSerializer(serializers.ModelSerializer):
-    # TODO сериалайзер для модели
-    pass
+    """ Used for listing ads, includes basic fields """
+
+    class Meta:
+        model = Ad
+        fields = ('id', 'title', 'price')
 
 
 class AdDetailSerializer(serializers.ModelSerializer):
-    # TODO сериалайзер для модели
-    pass
+    """ Used for detailed ad view, includes all fields and detailed information """
+    author_details = UserSerializer(source='author', read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Ad
+        fields = '__all__'

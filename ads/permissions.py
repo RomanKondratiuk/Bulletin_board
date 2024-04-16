f3e3203400adc3c0c7c455b0a661ddfb3a8c756e
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2024-04-16 19:42:47 -0400

    added permissions to model ad

--- ads/permissions.py
+++ ads/permissions.py
@@ -1,1 +1,21 @@
-# TODO здесь производится настройка пермишенов для нашего проекта
+from rest_framework.permissions import BasePermission
+
+
+class IsOwner(BasePermission):
+    message = 'You are not a owner!'
+
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj.author:
+            return True
+        return False
+
+
+class IsAdmin(BasePermission):
+    """
+    Allows access only to admin users.
+    """
+    def has_permission(self, request, view):
+        return request.user and request.user.role == 'admin'
+
+    def has_object_permission(self, request, view, obj):
+        return request.user and request.user.role == 'admin'
--- ads/views.py
+++ ads/views.py
@@ -1,5 +1,8 @@
 from rest_framework import pagination, viewsets, generics
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Ad
+from .permissions import IsOwner, IsAdmin
 from .serializers import AdSerializer, AdDetailSerializer
 
 
@@ -16,24 +19,28 @@ class AdListApiView(generics.ListAPIView):
 class AdCreateApiView(generics.CreateAPIView):
     """ creating an ad """
     serializer_class = AdSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class AdRetrieveApiView(generics.RetrieveAPIView):
     """ reading of  one ad """
     serializer_class = AdDetailSerializer
     queryset = Ad.objects.all()
+    permission_classes = [IsAuthenticated | IsOwner]
 
 
 class AdUpdateApiView(generics.UpdateAPIView):
     """ updating an ad """
     serializer_class = AdSerializer
     queryset = Ad.objects.all()
+    permission_classes = [IsAuthenticated | IsOwner | IsAdmin]
 
 
 class AdDestroyApiView(generics.DestroyAPIView):
     """ deleting an ad """
     serializer_class = AdSerializer
     queryset = Ad.objects.all()
+    permission_classes = [IsAuthenticated | IsOwner | IsAdmin]
 
 
 class CommentViewSet(viewsets.ModelViewSet):

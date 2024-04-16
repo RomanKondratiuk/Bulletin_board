7c2347db8fc972b86e32a03bfaafda2914724553
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2024-04-16 10:10:19 -0400

    added redoc url

--- config/urls.py
+++ config/urls.py
@@ -22,8 +22,9 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     path("users/", include("users.urls", namespace="users")),
+    path("api/redoc-tasks/", include("redoc.urls")),
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
